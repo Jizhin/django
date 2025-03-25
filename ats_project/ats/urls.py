bf5553de16ec34d68f978b03b7ffa72a025b1a4e
jsihin <jishin.cheruvattil@slots.com.kw>
2025-03-25 18:02:18 +0530

    df

--- ats_project/ats/urls.py
+++ ats_project/ats/urls.py
@@ -1,14 +1,16 @@
 from django.urls import path
 from .views import (
-    CandidateCreateAPIView,
-    CandidateUpdateAPIView,
-    CandidateDeleteAPIView,
-    CandidateSearchAPIView
+    CandidateCreateAPIView ,
+    CandidateUpdateAPIView ,
+    CandidateDeleteAPIView ,
+    CandidateSearchAPIView ,
+    LoginAPIView
 )
 
 urlpatterns = [
-    path('candidates/create/', CandidateCreateAPIView.as_view(), name='create-candidate'),
-    path('candidates/update/<int:pk>/', CandidateUpdateAPIView.as_view(), name='update-candidate'),
-    path('candidates/delete/<int:pk>/', CandidateDeleteAPIView.as_view(), name='delete-candidate'),
-    path('candidates/search/', CandidateSearchAPIView.as_view(), name='search-candidate'),
+    path('candidates/create/' , CandidateCreateAPIView.as_view() , name='create-candidate') ,
+    path('candidates/update/<int:pk>/' , CandidateUpdateAPIView.as_view() , name='update-candidate') ,
+    path('candidates/delete/<int:pk>/' , CandidateDeleteAPIView.as_view() , name='delete-candidate') ,
+    path('candidates/search/' , CandidateSearchAPIView.as_view() , name='search-candidate') ,
+    path('login/' , LoginAPIView.as_view() , name='login-api') ,
 ]
